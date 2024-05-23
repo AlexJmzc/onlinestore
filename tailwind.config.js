99e7f02c9b3d9e3bab9e63a8036c8fa087b96e56
Javier <alexanderjmzc@gmail.com>
2024-05-23 15:43:01 -0500

    Icons and styles add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: ["./src/*.{ts,tsx}", "./src/components/*.{ts,tsx}", "./src/pages/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          primary: "#1B1D24",
+          secondary: "#365A08",
+          tertiary: "#5D9D0B",
+          quaternary: "#72EB3A"
+      }
+    },
   },
-  plugins: [],
+  plugins: [addDynamicIconSelectors()],
 }
 
