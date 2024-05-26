5b791babcd96978d5359877f3fbccb4c73bb2879
Javier <alexanderjmzc@gmail.com>
2024-05-25 20:00:34 -0500

    Redistribution

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,25 +4,24 @@ export const Header = () => {
   return (
     <div className='flex bg-transparent w-screen py-6'>
         <div className='flex gap-x-2 w-1/2 ml-10'>
-            <div className='bg-primary rounded-3xl text-white'>
+            <div className='bg-primary rounded-3xl text-white px-4 py-2'>
                 STORE
             </div>
 
-            <div className='flex gap-x-3 bg-white'>
+            <div className='flex gap-x-6 bg-white px-6 py-2 rounded-3xl'>
                 <h2>Mouses</h2>
                 <h2>Keyboards</h2>
-                <h2>Mouse</h2>
                 <h2>Monitors</h2>
                 <h2>Headsets</h2>
             </div>
         </div>
 
         <div className='flex gap-x-2 w-1/2 justify-end mr-10 bg-transparent'>
-            <div className='bg-red-400 rounded-3xl'>
-                <span className="icon-[material-symbols--search]"></span>
+            <div className='flex items-center bg-white p-2 rounded-full'>
+                <span className="w-4 h-4 icon-[material-symbols--search]"></span>
             </div>
-            <div className='bg-red-400 rounded-3xl'>
-                <span className="icon-[mdi--cart-outline]"></span>
+            <div className='flex items-center bg-white p-2 rounded-3xl'>
+                <span className="w-4 h-4 icon-[mdi--cart-outline]"></span>
             </div>
         </div>
     </div>
