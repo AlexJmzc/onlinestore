0231f8eb3ddd9cfbd20d23d45d44bdfba1b727cb
Javier <alexanderjmzc@gmail.com>
2024-05-26 19:08:02 -0500

    Main page structure

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 export const Header = () => {
   return (
     <div className='flex bg-transparent w-screen py-6'>
-        <div className='flex gap-x-2 w-1/2 ml-10'>
-            <div className='bg-primary rounded-3xl text-white px-4 py-2'>
-                STORE
-            </div>
-
+        <div className='flex w-1/3 ml-10'>
             <div className='flex gap-x-6 bg-white px-6 py-2 rounded-3xl'>
                 <h2>Mouses</h2>
                 <h2>Keyboards</h2>
@@ -16,7 +12,11 @@ export const Header = () => {
             </div>
         </div>
 
-        <div className='flex gap-x-2 w-1/2 justify-end mr-10 bg-transparent'>
+        <div className='flex w-1/3 items-center justify-center'>
+            <h2>store</h2>
+        </div>
+
+        <div className='flex gap-x-2 w-1/3 justify-end mr-10 bg-transparent'>
             <div className='flex items-center bg-white p-2 rounded-full'>
                 <span className="w-4 h-4 icon-[material-symbols--search]"></span>
             </div>
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -7,6 +7,19 @@ export const Main = () => {
   return (
     <div className='h-screen' style={{ backgroundImage: `url(${Image})` }}>
         <Header/>
+        <div className='flex h-auto font-bold'>
+            <div className='w-1/2 text-7xl mt-20 ml-10'>
+              <div className='flex flex-col gap-y-4 w-3/4'>
+                  <h1 className='text-white'>LOS</h1>
+                  <h1 className='text-quaternary'>MEJORES</h1>
+                  <h1 className='text-white'>PRODUCTOS</h1>
+              </div>
+            </div>
+
+            <div className='w-1/2 text-white mt-20 text-3xl ml-10'>
+                COMPRA AHORA CON DESCUENTOS INCREÍBLES
+            </div>
+        </div>
     </div>
   )
 }
