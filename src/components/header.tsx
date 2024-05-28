08092a22c8f435b5c7553f8c292141649552e000
Javier <alexanderjmzc@gmail.com>
2024-05-27 20:12:06 -0500

    Header text

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <div className='flex bg-transparent w-screen py-6'>
         <div className='flex w-1/3 ml-10'>
-            <div className='flex gap-x-6 bg-white px-6 py-2 rounded-3xl'>
+            <div className='flex gap-x-6 bg-white px-6 py-2 rounded-3xl font-semibold'>
                 <h2>Mouses</h2>
                 <h2>Keyboards</h2>
                 <h2>Monitors</h2>
