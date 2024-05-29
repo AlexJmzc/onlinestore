5cfa50ad57b01f5a6649cc70c6f58019efbe4e45
Javier <alexanderjmzc@gmail.com>
2024-05-29 15:30:41 -0500

    Card item

---
+++ src/components/card.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import image from '../assets/img/headsets.jpg';
+
+export const Card = () => {
+    return (
+        <div className="relative flex items-end w-full max-w-[300px] h-[300px] p-7 bg-white shadow-lg transition-transform duration-500 ease-in-out rounded-[45px] hover:translate-y-5 group">
+            <img 
+                src={image} 
+                alt="Yuji" 
+                className="absolute inset-0 w-full h-full object-cover rounded-[40px]"
+            />
+            <div className="absolute inset-0 bg-gradient-to-b from-[rgba(208,195,195,0.5)] to-[rgb(227,16,16)] transition-opacity duration-500 opacity-0 rounded-[40px] group-hover:opacity-100"></div>
+            <div className="relative z-30 text-white opacity-0 transform translate-y-8 transition-opacity transition-transform duration-500 group-hover:opacity-100 group-hover:translate-y-0">
+                <h1 className="text-xl m-0">Yuji Itadori</h1>
+                <p className="tracking-wide text-sm my-2.5 leading-relaxed">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut alias
+                    nam dolorum natus maiores deserunt ipsum explicabo quia dolore
+                    rerum modi, perferendis unde a omnis praesentium totam inventore
+                    eum soluta!
+                </p>
+                <a href="/#" className="no-underline py-2 px-4 bg-white text-gray-700 text-sm font-bold rounded-full cursor-pointer transition-all duration-300 ease-in-out hover:shadow-lg hover:bg-gray-700 hover:text-white">
+                    Saber más
+                </a>
+            </div>
+        </div>
+    )
+}
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,24 +2,31 @@ import React from 'react';
 import { Header } from '../components/header';
 
 import Image from '../assets/img/main.jpg';
+import { Card } from '../components/card';
 
 export const Main = () => {
   return (
-    <div className='h-screen' style={{ backgroundImage: `url(${Image})` }}>
-        <Header/>
-        <div className='flex h-auto font-bold'>
-            <div className='w-1/2 text-7xl mt-20 ml-10'>
-              <div className='flex flex-col gap-y-4 w-3/4'>
-                  <h1 className='text-white'>LOS</h1>
-                  <h1 className='text-quaternary'>MEJORES</h1>
-                  <h1 className='text-white'>PRODUCTOS</h1>
+    <>
+      <div className='h-screen' style={{ backgroundImage: `url(${Image})` }}>
+          <Header/>
+          <div className='flex h-auto font-bold'>
+              <div className='w-1/2 text-7xl mt-20 ml-10'>
+                <div className='flex flex-col gap-y-4 w-3/4'>
+                    <h1 className='text-white'>LOS</h1>
+                    <h1 className='text-quaternary'>MEJORES</h1>
+                    <h1 className='text-white'>PRODUCTOS</h1>
+                </div>
               </div>
-            </div>
 
-            <div className='w-1/2 text-white mt-20 text-3xl ml-10'>
-                COMPRA AHORA CON DESCUENTOS INCREÍBLES
-            </div>
-        </div>
-    </div>
+              <div className='w-1/2 text-white mt-20 text-3xl ml-10'>
+                  COMPRA AHORA CON DESCUENTOS INCREÍBLES
+              </div>
+          </div>
+      </div>
+
+      <div className='h-max'>
+          <Card/>
+      </div>
+    </>
   )
 }
