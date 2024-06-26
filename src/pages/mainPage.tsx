546e2d60fad7119a34ebcb025fd330ec688b0de1
Javier <alexanderjmzc@gmail.com>
2024-06-26 18:29:10 -0500

    Main page

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,11 @@
+import { Principal } from '../components/principal/principal';
+import { Products } from '../components/products/products';
+
+export const MainPage = () => {
+  return (
+    <>
+      <Principal />
+      <Products />
+    </>
+  )
+}
