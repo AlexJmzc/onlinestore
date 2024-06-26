586b9a48f143ce1b56adb3e5b21e38ee41e698f2
Javier <alexanderjmzc@gmail.com>
2024-06-25 19:47:09 -0500

    Tailwind class bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ const { addDynamicIconSelectors } = require('@iconify/tailwind')
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/*.{ts,tsx}", "./src/components/*.{ts,tsx}", "./src/pages/*.{ts,tsx}"],
+  content: ['./src/*.{ts,tsx}', './src/components/*.{ts,tsx}', './src/components/*/*.{ts,tsx}', './src/pages/*.{ts,tsx}'],
   theme: {
     extend: {
       colors: {
-          primary: "#1B1D24",
-          secondary: "#365A08",
-          tertiary: "#5D9D0B",
-          quaternary: "#72EB3A"
+          primary: '#1B1D24',
+          secondary: '#365A08',
+          tertiary: '#5D9D0B',
+          quaternary: '#72EB3A'
       }
     },
   },
