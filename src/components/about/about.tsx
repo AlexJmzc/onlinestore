dd5ec85d91aa6d50d0d8bc71fdc8580cd8a66a3a
Javier <alexanderjmzc@gmail.com>
2024-06-25 20:15:54 -0500

    About component basic structure

---
+++ src/components/about/about.tsx
@@ -0,0 +1,12 @@
+
+export const About = () => {
+  return (
+    <div className='grid grid-cols-2 gap-x-6'>
+        <div className='bg-gray-950'>
+            <p>About us</p>
+        </div>
+
+        <img src='' alt='' />
+    </div>
+  )
+}
