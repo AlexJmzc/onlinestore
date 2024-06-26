00a02324cb960df78134230b75522da1b56e06c9
Javier <alexanderjmzc@gmail.com>
2024-06-25 20:15:42 -0500

    Logo and nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import Logo from '../assets/img/logo.png';
 
 export const Header = () => {
   return (
     <div className='flex bg-transparent w-screen py-6'>
         <div className='flex w-1/3 ml-10'>
-            <div className='flex gap-x-6 bg-white px-6 py-2 rounded-3xl font-medium'>
-                <h2>Mouses</h2>
-                <h2>Keyboards</h2>
-                <h2>Monitors</h2>
-                <h2>Headsets</h2>
+            <div className='flex gap-x-6 bg-white px-6 py-2 rounded-3xl font-medium h-max'>
+                <h2>Home</h2>
+                <h2>Products</h2>
+                <h2>About Us</h2>
+                <h2>Contact Us</h2>
             </div>
         </div>
 
         <div className='flex w-1/3 items-center justify-center'>
-            <h2>store</h2>
+            <img className='w-14 h-16' src={Logo} alt='Logo' />
         </div>
 
-        <div className='flex gap-x-2 w-1/3 justify-end mr-10 bg-transparent'>
+        <div className='flex gap-x-2 w-1/3 justify-end mr-10 bg-transparent h-max'>
             <div className='flex items-center bg-white p-2 rounded-full'>
                 <span className="w-4 h-4 icon-[material-symbols--search]"></span>
             </div>
