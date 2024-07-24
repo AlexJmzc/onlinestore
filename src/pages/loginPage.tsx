6db3f2d3da39e73d9492b15d8ba36e634249cdf4
Javier <alexanderjmzc@gmail.com>
2024-07-23 20:23:18 -0500

    Login page structure

---
+++ src/pages/loginPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../components/header';
+import { Login } from '../components/login/login';
+
+export const LoginPage = () => {
+  return (
+    <div>
+        <Header />
+        <Login />
+    </div>
+  )
+}
