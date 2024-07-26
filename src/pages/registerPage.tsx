c7d4cd80c84bbd116dde6e012aab2392a108462d
Javier <alexanderjmzc@gmail.com>
2024-07-25 20:09:31 -0500

    Register page structure

---
+++ src/pages/registerPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '../components/header';
+import { Register } from '../components/register/register';
+
+export const RegisterPage = () => {
+  return (
+    <>
+        <Header />
+        <Register />
+    </>
+  )
+}
