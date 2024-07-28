c6096642ee310109c8ce8e045aacb343741c5b4c
Javier <alexanderjmzc@gmail.com>
2024-07-27 20:02:05 -0500

    Login structure change

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -29,16 +29,14 @@ export const Login = () => {
   }
 
   return (
-    <div className='bg-primary flex justify-center items-center'>
-        <div className='bg-white p-10 rounded-md shadow-md w-1/2'>
+    <div className='bg-transparent flex justify-center items-center h-max mt-5'>
+        <div className='bg-white flex flex-col p-10 rounded-md gap-y-10 shadow-md w-1/2'>
             <input type="text" placeholder='Usuario' value={username} onChange={() => usernameChange} required/>
             <input type="password" placeholder='Contraseña' value={password} onChange={() => passwordChange} required/>
             <button className='p-4 bg-quaternary rounded-xl text-center'>Iniciar Sesión</button>
-        </div>
 
-        <div>
             <p className='text-center'>¿No tienes cuenta, regístrate?</p>
-        </div>
+        </div> 
     </div>
   )
 }
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../components/header';
 import { Login } from '../components/login/login';
 
+import Image from '../assets/img/main.jpg';
+
 export const LoginPage = () => {
   return (
-    <div>
+    <div className='h-screen' style={{ backgroundImage: `url(${Image})` }}>
         <Header />
         <Login />
     </div>
