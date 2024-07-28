2d8100f45aca0a8c5c331e0f1e92958e575e193d
Javier <alexanderjmzc@gmail.com>
2024-07-28 15:06:43 -0500

    Register page structure change

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,17 +1,15 @@
 
 export const Register = () => {
   return (
-    <div className='bg-primary flex justify-center items-center'>
-        <div className='bg-white p-10 rounded-md shadow-md w-1/2'>
+    <div className='bg-transparent flex justify-center items-center h-max mt-5'>
+        <div className='bg-white flex flex-col p-10 rounded-md gap-y-10 shadow-md w-1/2'>
             <input type="text" placeholder='Nombre Completo'/>
             <input type="email" placeholder='Correo'/>
             <input type="text" placeholder='Usuario'/>
             <input type="password" placeholder='Contraseña'/>
             <input type="password" placeholder='Confirmar Contraseña'/>
             <button className='p-4 bg-quaternary rounded-xl text-center'>Registrarse</button>
-        </div>
 
-        <div>
             <p className='text-center'>¿Ya tienes cuenta, inicia sesión?</p>
         </div>
     </div>
--- src/pages/registerPage.tsx
+++ src/pages/registerPage.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import { Header } from '../components/header';
 import { Register } from '../components/register/register';
 
+import Image from '../assets/img/main.jpg';
+
 export const RegisterPage = () => {
   return (
-    <>
+    <div className='h-screen' style={{ backgroundImage: `url(${Image})` }}>
         <Header />
         <Register />
-    </>
+    </div>
   )
 }
